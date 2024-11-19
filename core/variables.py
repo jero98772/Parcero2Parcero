50d23515162648e1282fccb356f9e50f41b08be9
jero98772 <jero98772@gmail.com>
2024-11-18 23:35:15 -0500

    add ip automatic set

--- core/variables.py
+++ core/variables.py
@@ -1,7 +1,26 @@
+import socket
+
+def get_host_ip():
+    """
+    Get the host's IP address automatically.
+    Returns:
+        str: The IP address of the host.
+    """
+    try:
+        # Create a socket to connect to a public server to determine the host IP
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))  # Connect to a public DNS server
+            host_ip = s.getsockname()[0]
+        return host_ip
+    except Exception as e:
+        return f"Error retrieving IP: {e}"
+
+
+
 BUFFER_SIZE = 4096
 TRACKER_PORT = 6000
 PEER_PORT = 5000
-TRACKER_IP = "192.168.1.18"  # Replace with the tracker's IP address
+TRACKER_IP = get_host_ip()#"192.168.0.12"  # Replace with the tracker's IP address
 SHARED_FOLDER = "templates"
 TRACKERS_FILE="data/trackers.txt"
 peer_list = []
